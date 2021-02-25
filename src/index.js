f8fac9af23786824a2c3b84acc5e6d5906e56346
uosl <regen@anche.no>
2021-02-25 15:37:48 +0000

    Improve integration with Bluegenes

--- src/index.js
+++ src/index.js
@@ -6,48 +6,40 @@ function main(el, service, imEntity, state, config) {
 		throw new Error('Call main with correct signature');
 	}
 
-	let organismID = '',
-		symbol = '';
-	const loading = document.createElement('h1');
-	loading.className = 'rootContainer';
+	let organismID = '';
+	let symbol = '';
+
+	const loading = document.createElement('h4');
 	loading.innerText = 'Loading...';
-	document.getElementById('yourDiv').appendChild(loading);
+	el.appendChild(loading);
+
 	queryData(imEntity.Gene.value, service.root)
 		.then(data => {
-			const main = document.getElementById('yourDiv');
-			main.removeChild(loading);
+			el.removeChild(loading);
 			if (data.length) organismID = data[0].organism.taxonId;
 			symbol = data.reduce(
 				(prevVal, currVal, idx) =>
 					idx == 0 ? currVal.symbol : prevVal + '|' + currVal.symbol,
 				''
 			);
-		})
-		.then(() => {
 			const iframeSrc = `http://www.esyn.org/app.php?embedded=true&query=${symbol}&organism=${organismID}&includeInteractors=false&source=biogrid`;
 			const rootDiv = document.createElement('div');
 			rootDiv.className = 'rootContainer';
-			const title = document.createElement('h2');
-			title.style.textAlign = 'center';
-			title.innerText = 'ESYN VIEWER';
-			rootDiv.appendChild(title);
 			const iframe = document.createElement('iframe');
-			iframe.id = 'frame';
 			iframe.className = 'seamless';
 			iframe.scrolling = 'no';
-			iframe.style.width = '100%';
-			iframe.style.height = '92%';
+			iframe.frameBorder = 0;
+			iframe.width = '100%';
+			iframe.height = '100%';
 			iframe.src = iframeSrc;
 			rootDiv.appendChild(iframe);
-			document.getElementById('yourDiv').appendChild(rootDiv);
+			el.appendChild(rootDiv);
 		})
 		.catch(() => {
-			const main = document.getElementById('yourDiv');
-			main.removeChild(loading);
-			const rootDiv = document.createElement('h1');
-			rootDiv.className = 'rootContainer';
-			rootDiv.innerText = 'Something went wrong!';
-			document.getElementById('yourDiv').appendChild(rootDiv);
+			el.removeChild(loading);
+			const error = document.createElement('h4');
+			error.innerText = 'Failed to load esyN';
+			el.appendChild(error);
 		});
 }
 
